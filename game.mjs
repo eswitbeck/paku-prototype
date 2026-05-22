81994e854b9ca1db4e918b00706adc6209404531
Esther Witbeck <esther.witbeck@gmail.com>
2026-05-21 22:01:22 -0700

    Draw most of the game owl (check timings, score, ghost behavior)

--- game.mjs
+++ game.mjs
@@ -2,8 +2,132 @@ import previewFrames from './previewFrames.mjs';
 
 const SPECIAL_PIP_DIST_FROM_PAKU = 3;
 
+class Ghost {
+  static color = [255,0,0,1];
+  static weakColor = [0,0,255,1];
+
+  static weakSpeedMod = 0.7;
+  static seesPakuSpeedMod = 1.2;
+
+  framesPerMove = 12;
+  location = 12;
+  direction = 1;
+  state = 'alive'; // alive | weak | dead
+  visionDepth = 5;
+  
+  deadAnimFrame = 0;
+  seesPaku = false;
+
+  deadTimer = 0;
+  weakTimer = 0;
+
+  difficulty = 0;
+
+  update(frameNum, pakuLocation) {
+    this.updateStateTimers();
+    this.checkPaku(pakuLocation);
+    if (0 === Math.floor(frameNum % this.getFramesPerMove())) {
+      this.location = (this.location + this.direction + 16) % 16;
+    }
+  }
+
+  // dedup?
+  die() {
+    this.state = 'dead';
+    this.deadTimer = 16 * 12;
+    this.seesPaku = false;
+  }
+
+  weak() {
+    this.state = 'weak';
+    if (this.seesPaku) {
+      this.seesPaku = false;
+    }
+    this.weakTimer = 12 * 12;
+  }
+
+  getFramesPerMove() {
+    let speed = this.framesPerMove;
+
+    if ('weak' === this.state) {
+      speed /= Ghost.weakSpeedMod;
+    } else if ('alive' === this.state) {
+      if (this.seesPaku) {
+        speed /= Ghost.seesPakuSpeedMod;
+      }
+
+      speed -= 0.35 * this.difficulty;
+    }
+
+    return speed;
+  }
+
+  updateStateTimers() {
+    switch (this.state) {
+        // consolidate?
+      case 'weak':
+        if (--this.weakTimer === 0) {
+          this.state = 'alive';
+        }
+        break;
+      case 'dead':
+        if (--this.deadTimer === 0) {
+          this.state = 'alive';
+        }
+        break;
+    }
+  }
+
+  checkPaku(frameNum, pakuLocation) {
+    if (0 === Math.floor(frameNum % this.getFramesPerMove())) {
+
+      const aheadDist = (pakuLocation - this.location + 16) % 16;
+      const behindDist = (this.location - pakuLocation + 16) % 16;
+
+      const pakuAhead = aheadDist > 0 && aheadDist <= this.visionDepth;
+      const pakuBehind = behindDist > 0 && behindDist <= this.visionDepth;
+
+      if (pakuAhead || pakuBehind) {
+        if (!this.seesPaku) {
+          this.seesPaku = true;
+        }
+
+        // turn to face if alive; run if dead
+        // otherwise wander
+        if (
+          (this.direction < 0 && pakuAhead && 'alive' === this.state) ||
+          (this.direction < 0 && pakuBehind && 'weak' === this.state)
+        ) {
+          this.direction = 1;
+        } else if (
+          (this.direction > 0 && pakuBehind && 'alive' === this.state) ||
+          (this.direction > 0 && pakuAhead && 'weak' === this.state)
+        ) {
+          this.direction = -1;
+        }
+      }
+    }
+  }
+
+  drawWheel(wheelBuffer) {
+    switch (this.state) {
+      case 'alive': 
+        wheelBuffer[this.location] = Ghost.color;
+        break;
+      case 'weak': 
+        wheelBuffer[this.location] = Ghost.weakColor;
+        break;
+      case 'dead': 
+        // play animation`
+        // TODO
+        wheelBuffer[this.location] = [0,255,255,1];
+        break;
+    }
+  }
+}
+
 class Paku {
-  framesPerMove = 5;
+  framesPerMove = 14;
   location = 4;
   direction = 0;
   directionIntent = null;
@@ -15,7 +139,7 @@ class Paku {
       this.directionIntent = inputBuffer.direction;
     }
 
-    if (0 === frameNum % this.framesPerMove) {
+    if (0 === Math.floor(frameNum % this.framesPerMove)) {
       if (null !== this.directionIntent) {
         this.direction = Paku.intentToDir[this.directionIntent] || 0;
       }
@@ -24,7 +148,6 @@ class Paku {
   }
 
   drawWheel(wheelBuffer) {
-    // TODO super paku?
     wheelBuffer[this.location] = Paku.color;
   }
 }
@@ -35,24 +158,62 @@ class Game {
   pips = new Array(16).fill(true);
   specialPipLocation;
   score = 0;
+  over = false;
   
   constructor() {
     this.paku = new Paku();
+    this.ghost = new Ghost();
   }
 
   setSpecialPip() {
-    const range = 16 - (2 * SPECIAL_PIP_DIST_FROM_PAKU);
-    const loc = Math.round(Math.random() * range) + this.paku.location;
-    this.specialPipLocation = loc % 16;
+    const valid = [];
 
+    for (let i = 0; i < 16; i++) {
+      const dist = (i - this.paku.location + 16) % 16;
+
+      if (
+        this.pips[i] &&
+        dist >= SPECIAL_PIP_DIST_FROM_PAKU &&
+        dist <= 16 - SPECIAL_PIP_DIST_FROM_PAKU
+      ) {
+        valid.push(i);
+      }
+    }
+
+    this.specialPipLocation =
+      valid[Math.floor(Math.random() * valid.length)];
   }
 
   update(frameNum, inputBuffer) {
+    if (this.over) {
+      // play animation then head to poss high score setting
+      return null;
+    }
+
     this.paku.update(frameNum, inputBuffer);
+
+    this.ghost.update(frameNum);
+
     if (this.pips[this.paku.location]) {
       this.score++;
       this.pips[this.paku.location] = false;
-      // if special pip
+      if (this.paku.location === this.specialPipLocation) {
+        if ('alive' === this.ghost.state) {
+          this.ghost.weak();
+        }
+      }
+    }
+
+    if (this.paku.location === this.ghost.location) {
+      switch (this.ghost.state) {
+        case 'alive':
+          this.over = true;
+          break;
+        case 'weak':
+          this.ghost.die();
+          this.score += 50;
+          break;
+      }
     }
 
     // reset pips
@@ -61,6 +222,7 @@ class Game {
         this.pips[i % 16] = true;
       }
       this.setSpecialPip();
+      this.ghost.difficulty += 1;
     }
 
     return null;
@@ -69,10 +231,16 @@ class Game {
   drawWheel(wheelBuffer) {
     for (let i = 0; i < 16; i++) {
       if (this.pips[i]) {
-        wheelBuffer[i] = [255,255,255,1];
+        if (i === this.specialPipLocation) {
+          wheelBuffer[i] = [230,255,255,1];
+        } else {
+          wheelBuffer[i] = [255,255,255,0.5];
+        }
+
       }
     }
     this.paku.drawWheel(wheelBuffer);
+    this.ghost.drawWheel(wheelBuffer);
   }
 
   drawGrid(gridBuffer) {
@@ -100,7 +268,6 @@ class Preview {
     if (0 === (frameNum % 5)) {
       this.dir += Preview.update[this.loggedInputDir] || 0;
       this.loggedInputDir = null;
-      console.log(this.dir);
     }
 
     if (17 === this.dir) {
